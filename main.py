cd790e522bf8e2a52a27e3a037d860dce20740d4
hermanvadym <vadymherman@gmail.com>
2025-08-02 21:48:03 +0200

    added ingame borders

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pygame
 
 def find_screen_center(screen):
-    screen_width = screen.get_width() / 2
-    screen_height = screen.get_height() / 2
-    return screen_width, screen_height
+    screen_width_center = screen.get_width() / 2
+    screen_height_center = screen.get_height() / 2
+    return screen_width_center, screen_height_center
 
 def main():
     print("It's nice to see another soul that likes to be challenged!")
@@ -15,8 +15,10 @@ def main():
     running = True
     dt = 0 # stores the time in seconds since last frame, used for smooth movement
     
-    player_screen_width, player_screen_height = find_screen_center(screen)
-    player_car = pygame.Rect(player_screen_width, player_screen_height + 350, 100, 120)
+    player_screen_width_center, player_screen_height_center = find_screen_center(screen)
+    player_car = pygame.Rect(player_screen_width_center, player_screen_height_center + 350, 100, 120)
+    
+    #left_line = w3
    
 
     while running:
@@ -31,14 +33,17 @@ def main():
         pygame.draw.rect(screen, "red", player_car)
 
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:
-            player_car.y -= 300 * dt
-        if keys[pygame.K_s]:
-            player_car.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_car.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_car.x += 300 * dt
+        if keys[pygame.K_w] and player_car.y > 20:
+            player_car.y -= 1300 * dt
+        if keys[pygame.K_s] and player_car.y < 850:
+            player_car.y += 1300 * dt
+        if keys[pygame.K_a] and player_car.x > 5:
+            player_car.x -= 1300 * dt
+        if keys[pygame.K_d] and player_car.x < 1798:
+            player_car.x += 1300 * dt
+        
+        #print(player_car.x)
+
 
         # flip() the display to put your work on screen
         pygame.display.flip() # show the frame
